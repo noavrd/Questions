5b44230eca5d1f7e0b8da41cff5eb7966a06f0e5
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-18 18:52:45 +0200

    fix question 9

--- q9.js
+++ q9.js
@@ -1,10 +1,10 @@
 for (let i = 1; i < 10; i++){
     for (let j = 0; j <10; j++){
         for (let m = 0; m < 10; m++){
-            let sum = ((i*3) + (j*3) + (m*3));
+            let sum = (Math.pow(i,3) + Math.pow(j,3) + Math.pow(m,3));
             let arm = ((i*100) + (j*10) + (m));
             if (arm === sum) {
-                console.log(sum);
+                console.log(arm);
             }
         }
     }
