6a0bd9473c3f996046d9272c52bb5a2224268e08
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-18 16:35:27 +0200

    Answer for question 4
    Answer for question 4 in q4.js

---
+++ q4.js
@@ -0,0 +1,12 @@
+let x = 1, y= 5, z= 10, n = 8, m =2;
+if (x > y && x > z && x > n && x > m) {
+    alert(x);
+} else if (y > x && y > z && y > n && y >m) {
+    alert(y);
+} else if (z > x && z > y && z > n && z >m) {
+    alert(z);
+} else if (n > x && n > y && n > z && n >m) {
+    alert(n);
+} else {
+    alert(m);
+}
