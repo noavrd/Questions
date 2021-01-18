c06baf9c102fc2cc37c5130fd8506ef192b41413
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-18 17:12:05 +0200

    Answer for question 6
    Answer for question 6 in q6.js

---
+++ q6.js
@@ -0,0 +1,13 @@
+ let x = 80, y = 77, z = 88, m = 95, t = 68;
+ let ave = (x+y+z+m+t)/5;
+ if (ave < 60) {
+     console.log("F");
+ } else if (ave < 70) {
+    console.log("D");
+ } else if (ave < 80) {
+    console.log("C");
+ } else if (ave < 90) {
+    console.log("B");
+ } else {
+    console.log("A");
+ }
