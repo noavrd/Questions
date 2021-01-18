6133e99f52882d2288c1931dc16f2b244d774347
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-18 16:12:09 +0200

    Answer for question 1
    Answer for question 1 in q2.js

---
+++ q2.js
@@ -0,0 +1,8 @@
+let num1 = 3, num2 =-7, num3 = 2;
+let product = Number(num1) * Number(num2) * Number(num3);
+if (product < 0) {
+    alert("The sigh is -");
+}
+else {
+    alert("The sigh is +");
+}
