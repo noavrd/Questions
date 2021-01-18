36eed8a874cdb50a41d7d310691da4d7e4e47137
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-18 15:49:51 +0200

    Answer forquestion 1
    Answer forquestion 1 in q1.js

---
+++ q1.js
@@ -0,0 +1,8 @@
+let num1 = Number(prompt("insert first number"));
+let num2 = Number(prompt("insert second number"));
+if (num1 > num2) {
+    console.log(alert(num1));
+} else {
+    console.log(alert(num2));
+
+}
